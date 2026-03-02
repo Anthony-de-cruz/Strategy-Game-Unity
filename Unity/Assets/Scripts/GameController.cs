f8e26156c1846f600fc4b3ff5c29cd96818e9eab
Anthony de Cruz <anthonydecruz@proton.me>
2026-03-02 11:42:27 +0000

    Added unit factory.

--- Unity/Assets/Scripts/GameController.cs
+++ Unity/Assets/Scripts/GameController.cs
@@ -3,20 +3,49 @@ using UnityEngine;
 using GameLogic;
 using GameLogic.Events;
 
+/// <summary>
+/// 
+/// </summary>
 public class GameController : MonoBehaviour
 {
     private EventBus _eventBus;
     private GameState _gameState;
 
-    void Start()
+    /// <summary>
+    /// Called on script load.
+    /// </summary>
+    void Awake()
     {
-        _eventBus = new EventBus(); 
-        _gameState = new GameState(1, 2, _eventBus);
+        _eventBus = new EventBus();
+        _eventBus.Subscribe<UnitDamagedEvent>(OnUnitDamaged);
+
+        _gameState = new GameState(20, 20, _eventBus);
+
+        for (uint i = 1; i < 3; i++) {
+            _gameState.AddUnit(new Unit(i, UnitTeam.Blue, UnitType.Infantry, 10, _eventBus));
+        }
+
+        _gameState.Map[2][2].UnitId = 1;
+
+        _gameState.TryGetUnit(1, out Unit unit);
+
+        unit.Strength -= 2;
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Called once per frame.
+    /// </summary>
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Handles raised <see cref="UnitDamagedEvent"/>.
+    /// </summary>
+    /// <param name="gameEvent"></param>
+    void OnUnitDamaged(UnitDamagedEvent gameEvent)
+    {
+        Debug.Log("DAMAGED EVENT");
     }
 }
